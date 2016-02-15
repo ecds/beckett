0e70701758311680944dbccc7242347fa7cad525
Sara Palmer <sara.palmer@emory.edu>
2016-02-15 11:25:19 -0500

    remove autodiscover

--- beckett/urls.py
+++ beckett/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import ListView
 
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
