32f6f67b5ead50ab866944d50295f9577ee009d3
Sara Palmer <sara.palmer@emory.edu>
2016-02-15 11:11:29 -0500

    admin autodiscover

--- beckett/urls.py
+++ beckett/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import ListView
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^people/', include('beckett.apps.people.urls', namespace="persons")),
