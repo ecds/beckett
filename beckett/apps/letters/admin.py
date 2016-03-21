00722bfa5717468bc7bff4aae39765dd001f701f
Joanna <joanna@ecds-staffmac-01.library.emory.ddns>
2016-03-21 14:25:31 -0400

    template input

--- beckett/apps/letters/admin.py
+++ beckett/apps/letters/admin.py
@@ -16,7 +16,7 @@ class LetterAdmin(admin.ModelAdmin):
             'fields': ('year', 'month', 'day', 'primary_language')}),
         ('Recipients', {
             'classes': ('collapse',),
-            'fields': ('recipients', 'recipients_input')}),
+            'fields': ('recipients', 'recipients_excel')}),
         ('Physical Description', {
             'classes': ('collapse',),
             'fields': ('physical_description', 'leaves_sides', 'envelope', 'postmark')}),
@@ -25,25 +25,25 @@ class LetterAdmin(admin.ModelAdmin):
             'fields': ('key_terms',)}),
         ('People Mentioned', {
             'classes': ('collapse',),
-            'fields': ('people', 'people_input')}),
+            'fields': ('people', 'people_excel')}),
         ('Publishers/Agents/Producers', {
             'classes': ('collapse',),
-            'fields': ('publishers', 'publishers_input')}),
+            'fields': ('publishers', 'publishers_excel')}),
         ('Repositories', {
             'classes': ('collapse',),
-            'fields': ('repository', 'repository_input', 'owner_input')}),
+            'fields': ('repository', 'repository_excel', 'owner_excel')}),
         ('Places', {
             'classes': ('collapse',),
-            'fields': ('places', 'places_input', 'place_written', 'place_written_input', 'place_sent', 'place_sent_input')}),
+            'fields': ('places', 'places_excel', 'place_written', 'place_written_excel', 'place_sent', 'place_sent_excel')}),
         ('SB Writing', {
             'classes': ('collapse',),
-            'fields': ('sb_writing', 'sb_writing_input')}),
+            'fields': ('sb_writing', 'sb_writing_excel')}),
         ('SB Publications and Productions', {
             'classes': ('collapse',),
-            'fields': ('sb_publications', 'sb_publications_input', 'edition_notes', 'sb_productions', 'sb_productions_input', 'edition_notes2')}),
+            'fields': ('sb_publications', 'sb_publications_excel', 'edition_notes', 'sb_productions', 'sb_productions_excel', 'edition_notes2')}),
         ('Translations', {
             'classes': ('collapse',),
-            'fields': ('self_translations','self_translations_input','edition_notes3','translations', 'translations_input', 'edition_notes4')}),
+            'fields': ('self_translations','self_translations_excel','edition_notes3','translations', 'translations_excel', 'edition_notes4')}),
         ('Censorship', {
             'classes': ('collapse',),
             'fields': ('censorship',)}),
@@ -52,7 +52,7 @@ class LetterAdmin(admin.ModelAdmin):
             'fields': ('publication', 'volume','previous_publication', 'place_of_previous_publication',)}),
         ('SB Reading', {
             'classes': ('collapse',),
-            'fields': ('sb_reading', 'sb_reading_input')}),
+            'fields': ('sb_reading', 'sb_reading_excel')}),
         ('SB Activities', {
             'classes': ('collapse',),
             'fields': ('events_attended','sports','sb_health')}),
@@ -60,6 +60,6 @@ class LetterAdmin(admin.ModelAdmin):
             'classes': ('collapse',),
             'fields': ('additional_information',)}),
     )
-    search_fields = ('year', 'primary_language', 'recipients_input', 'physical_description', 'leaves_sides', 'envelope', 'postmark', 'places_input', 'place_written_input', 'place_sent_input', 'additional_information', 'events_attended','sports','sb_health','self_translations_input','translations_input', 'sb_publications_input', 'sb_writing_input', 'sb_productions_input')
+    search_fields = ('year', 'primary_language', 'recipients_excel', 'physical_description', 'leaves_sides', 'envelope', 'postmark', 'places_excel', 'place_written_excel', 'place_sent_excel', 'additional_information', 'events_attended','sports','sb_health','self_translations_excel','translations_excel', 'sb_publications_excel', 'sb_writing_excel', 'sb_productions_excel')
 
 admin.site.register(Letter, LetterAdmin)
